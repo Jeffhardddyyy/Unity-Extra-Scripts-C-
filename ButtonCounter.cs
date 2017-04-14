78cf3ea8af52f7d4a20f717a4818e6025d546703
Dillion <dillionpiazza@gmail.com>
2017-04-14 09:26:49 -0400

    Create ButtonCounter.cs

---
+++ ButtonCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class ButtonCounter : MonoBehaviour
+{
+    void Update()
+    {//every frame
+        if (Input.GetKeyDown(KeyCode.Escape))           //Change "Escape" to whatever button you want it to be.
+        {//if Escape is pressed
+
+            //code here
+        }
+    }
+}
+
